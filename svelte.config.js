d3d47b0b92f67729e8097e0b6a397430b1e65b4d
Onur Akpolat <onur.akpolat@gmail.com>
2026-02-01 17:47:37 +0000

    fix: remove base path (custom domain whatsmydna.xyz is primary)

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
@@ -14,7 +12,7 @@ const config = {
       precompress: true
     }),
     paths: {
-      base: dev ? '' : '/whatsmydna'
+      base: ''
     }
   }
 };
